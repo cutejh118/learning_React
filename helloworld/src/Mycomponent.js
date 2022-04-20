4ae65a3fa0d4d08dc73746f5172301b45add1fe8
cutejh118 <cooljh118@gmail.com>
2022-04-20 20:16:22 +0900

    props

---
+++ helloworld/src/Mycomponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Mycomponent = ({name}) => {
+
+    //const {name} = props; //비구조화
+
+    return (
+        <div>
+            안녕하세요 컴포넌트 이해<br></br>
+            {name}입니다.
+        </div>
+    );
+};
+
+Mycomponent.defaultProps = {
+    name : '기본 값' 
+};
+
+
+export default Mycomponent;
