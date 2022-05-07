6cdbf0e5e5e8c57d35b3b172a0efcd80552f9ef7
cutejh118 <cooljh118@gmail.com>
2022-05-07 17:08:15 +0900

    useCallback

--- helloworld/src/Average.js
+++ helloworld/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 const getAverage = numbers =>{
     console.log('평균값 계산중...');
@@ -11,17 +11,14 @@ const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e =>{
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-
-    };
-    const onInsert = e =>{
+    }, []);
+    const onInsert = useCallback(() => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-
-    };
-
+    }, [number, list]);
     const avg = useMemo(() => getAverage(list), [list] );
 
     return (
