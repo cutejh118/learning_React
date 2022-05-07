6ef85880390b64667a0cfadb6adf467b38e12700
cutejh118 <cooljh118@gmail.com>
2022-05-07 12:09:18 +0900

    Event

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Mycomponent from "./Mycomponent";
 import Say from "./Say";
+import EventPractice from "./EventPractice";
 function App() {
   return(
     <div>
         안녕 <Mycomponent name="여정희" ></Mycomponent>
         <Say></Say>
+        <EventPractice></EventPractice>
     </div>
 
   ); 
